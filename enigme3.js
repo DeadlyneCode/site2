a8519a2bcc509783a284c930f6ee42e494793e6e
Deadlyne <114301388+DeadlyneCode@users.noreply.github.com>
2024-06-24 15:50:53 +0200

    dgdfg

--- enigme3.js
+++ enigme3.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('checkCode').addEventListener('click', () => {
         const code = document.getElementById('codeInput').value.trim();
-        if (code === '1234') { // Exemple de code correct
+        if (code === '5798') { // Exemple de code correct
             // Afficher l'image et masquer le formulaire
             imageAndText.style.display = 'block';
             section.style.display = 'none';
