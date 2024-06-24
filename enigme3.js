12bf557b249636285f040b4797b388aaa8396a84
Deadlyne <114301388+DeadlyneCode@users.noreply.github.com>
2024-06-24 15:37:51 +0200

    dhgfghfghf

--- enigme3.js
+++ enigme3.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const imageAndText = document.getElementById('imageAndText');
+    const section = document.querySelector('.section');
+
     document.getElementById('checkCode').addEventListener('click', () => {
         const code = document.getElementById('codeInput').value.trim();
         if (code === '1234') { // Exemple de code correct
-            window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // Lien vers la vidéo YouTube
+            // Afficher l'image et masquer le formulaire
+            imageAndText.style.display = 'block';
+            section.style.display = 'none';
         } else {
             alert('Code incorrect. Réessayez.');
         }
